b12f1c3eb0d7fd5b5b25b0232af4ab02715d90b2
XxRexRaptorxX <Raptor-Simon@web.de>
2021-12-30 01:00:13 +0100

    Initial commit

---
+++ src/main/java/xxrexraptorxx/cdl/main/References.java
@@ -0,0 +1,9 @@
+package xxrexraptorxx.cdl.main;
+
+public class References {
+
+    public static final String NAME = "Customized Dungeon Loot";
+    public static final String MODID = "cdl";
+    public static final String VERSION = "2.0.0";
+
+}
---
+++ src/main/java/xxrexraptorxx/cdl/main/package-info.java
@@ -0,0 +1,6 @@
+package xxrexraptorxx.cdl.main;
+
+/**
+ * @author XxRexRaptorxX (RexRaptor)
+ * @projectPage https://www.curseforge.com/minecraft/mc-mods/customized-dungeon-loot
+ **/
