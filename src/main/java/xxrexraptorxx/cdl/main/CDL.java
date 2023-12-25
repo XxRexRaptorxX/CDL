4773f614c5fad9881b9e18a1d5ba040699ac663a
XxRexRaptorxX <Raptor-Simon@web.de>
2023-12-25 01:32:03 +0100

    Port for Neoforge

--- src/main/java/xxrexraptorxx/cdl/main/CDL.java
+++ src/main/java/xxrexraptorxx/cdl/main/CDL.java
@@ -1,10 +1,10 @@
 package xxrexraptorxx.cdl.main;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.neoforge.common.NeoForge;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
@@ -21,7 +21,6 @@ public class CDL {
     public static final Logger LOGGER = LogManager.getLogger();
 
     public CDL() {
-        IEventBus forgeBus = MinecraftForge.EVENT_BUS;
         IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         modBus.addListener(this::setup);
@@ -31,7 +30,7 @@ public class CDL {
 
 
     private void setup(final @NotNull FMLCommonSetupEvent event) {
-        MinecraftForge.EVENT_BUS.addListener(LootTableInjection::onChestLootLoad);
+        NeoForge.EVENT_BUS.addListener(LootTableInjection::onChestLootLoad);
     }
 
 }
--- src/main/java/xxrexraptorxx/cdl/utils/Config.java
+++ src/main/java/xxrexraptorxx/cdl/utils/Config.java
@@ -1,25 +1,18 @@
 package xxrexraptorxx.cdl.utils;
 
-import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.common.ModConfigSpec;
 
-import java.util.List;
-
-@Mod.EventBusSubscriber
 public class Config {
 
     public static final String CATEGORY_GENERAL = "general";
 
-    public static ForgeConfigSpec CLIENT_CONFIG;
-    public static ForgeConfigSpec COMMON_CONFIG;
+    public static ModConfigSpec CLIENT_CONFIG;
+    public static ModConfigSpec COMMON_CONFIG;
 
-    public static ForgeConfigSpec.BooleanValue UPDATE_CHECKER;
-    public static ForgeConfigSpec.BooleanValue PATREON_REWARDS;
-
-    public static ForgeConfigSpec.DoubleValue UPCYCLE_CHANCE;
-    public static ForgeConfigSpec.ConfigValue<List<String>> COMPOSTABLE_ITEMS;
+    public static ModConfigSpec.BooleanValue UPDATE_CHECKER;
+    public static ModConfigSpec.BooleanValue PATREON_REWARDS;
 
 
     public static void init() {
@@ -32,7 +25,7 @@ public class Config {
 
 
     public static void initClient() {
-        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+        ModConfigSpec.Builder builder = new ModConfigSpec.Builder();
 
         builder.comment("General").push(CATEGORY_GENERAL);
         UPDATE_CHECKER = builder.comment("Activate the Update-Checker").define("update-checker", true);
@@ -42,21 +35,10 @@ public class Config {
     }
 
     public static void initCommon() {
-        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+        ModConfigSpec.Builder builder = new ModConfigSpec.Builder();
 
         builder.comment("General").push(CATEGORY_GENERAL);
-        UPCYCLE_CHANCE = builder.comment("The chance for an item to add a level of compost").defineInRange("upcycle_chance", 0.3F, 0.1F, 1.0F);
         PATREON_REWARDS = builder.comment("Enables ingame rewards on first spawn for Patreons").define("patreon_rewards", true);
-
-        /** TODO
-        COMPOSTABLE_ITEMS = builder.comment("A list with all compostable items [modid:item]").define("compostable_items", new ArrayList<>(Arrays.asList(
-                Items.ROTTEN_FLESH.getRegistryName().toString(),
-                Items.SPIDER_EYE.getRegistryName().toString(),
-                Items.FERMENTED_SPIDER_EYE.getRegistryName().toString(),
-                Items.POISONOUS_POTATO.getRegistryName().toString(),
-                Items.RABBIT_FOOT.getRegistryName().toString()
-        )));
-        **/
         builder.pop();
 
         COMMON_CONFIG = builder.build();
